e9a35385cca12f3aa50859b3e1d3c7206f7f04b9
filipmaciejak <fimaciejak@gmail.com>
2021-12-20 14:31:21 +0100

    code review changes

--- task_0/CString.c
+++ task_0/CString.c
@@ -5,12 +5,13 @@
 typedef struct CString
 {
     char* string;
-    int length;
+    size_t length;
 } CString;
 
 CString* cstring_new(const char* str)
 {
-    CString* result = malloc(sizeof(CString));
+    CString* result = (CString*) malloc(sizeof(CString));
+    if (result == NULL) return NULL;
     result->length = strlen(str) + 1; // include the \0 character
     result->string = malloc(result->length * sizeof(char));
     strcpy(result->string, str);
@@ -19,17 +20,20 @@ CString* cstring_new(const char* str)
 
 int cstring_length(CString* cstring)
 {
+    if (cstring == NULL) return 0;
     return cstring->length;
 }
 
 char cstring_getchar(CString* cstring, int index)
 {
-    if (index < 0 || index >= cstring->length) return -1;
+    if (cstring == NULL) return '\0';
+    if (index < 0 || index >= cstring->length) return '\0';
     return cstring->string[index];
 }
 
 int cstring_setchar(CString* cstring, int index, char c)
 {
+    if (cstring == NULL) return -1;
     if (index < 0 || index >= cstring->length) return -1;
     cstring->string[index] = c;
     return 0;
@@ -37,11 +41,13 @@ int cstring_setchar(CString* cstring, int index, char c)
 
 const char* cstring_getchars(CString* cstring)
 {
+    if (cstring == NULL) return NULL;
     return cstring->string;
 }
 
 void cstring_free(CString* cstring)
 {
+    if (cstring == NULL) return;
     free(cstring->string);
     free(cstring);
 }
@@ -51,13 +57,16 @@ int main()
     // tests
     CString* cstring = cstring_new("Ala ma kota.");
     assert(cstring_length(cstring) == 13);
+    assert(cstring_length(NULL) == 0);
     assert(cstring_getchar(cstring, 0) == 'A');
     assert(cstring_getchar(cstring, 1) == 'l');
     assert(cstring_getchar(cstring, 2) == 'a');
-    cstring_setchar(cstring, 0, 'O');
+    assert(cstring_setchar(cstring, 0, 'O') == 0);
+    assert(cstring_setchar(NULL, 0, 'O') == -1);
     assert(cstring_getchar(cstring, 0) == 'O');
-    assert(cstring_getchar(cstring, 100) == -1);
-    assert(cstring_getchar(cstring, -100) == -1);
+    assert(cstring_getchar(cstring, 100) == '\0');
+    assert(cstring_getchar(cstring, -100) == '\0');
+    assert(cstring_getchar(NULL, 2) == '\0');
 
     cstring_free(cstring);
     return 0;
